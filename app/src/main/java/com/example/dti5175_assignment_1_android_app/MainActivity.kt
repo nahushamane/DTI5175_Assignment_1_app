3fbc678bd25380c2c22dc83e7ac32201d5b9f2dd
NAHUSH-AMD\nahushamane <nahushamane@gmail.com>
2023-01-25 16:13:59 -0500

    intial commit

---
+++ app/src/main/java/com/example/dti5175_assignment_1_android_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dti5175_assignment_1_android_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
